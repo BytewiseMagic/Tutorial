72be44ffa8a0056d4563402fe417772a8355cc45
BytewiseMagic <michellelrosenstock@gmail.com>
2022-10-20 19:29:45 -0400

    added std name space, removed std from thee cout statements

---
+++ CPPtest/XCode Workspace/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  CPPtest
+//
+//
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Hello, World!\n";
+    cout << "trying this out!\n";
+    return 0;
+}
